e2a5665b879e7e53bd8cfd0d18ddb2d70806b6dc
Mark Zorkhin <mark.zorikhin@dif.tech>
2025-05-22 14:13:25 +0300

    v1.0.10

--- src/commonMain/kotlin/hnau/common/projector/app/AppProjector.kt
+++ src/commonMain/kotlin/hnau/common/projector/app/AppProjector.kt
@@ -9,15 +9,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
-import arrow.core.Either
-import hnau.common.kotlin.Loadable
-import hnau.common.kotlin.coroutines.mapWithScope
 import hnau.common.kotlin.ifNull
 import hnau.common.kotlin.ifTrue
-import hnau.common.kotlin.map
-import hnau.common.kotlin.valueOrElse
 import hnau.common.model.ThemeBrightness
-import hnau.common.model.app.AppContext
 import hnau.common.model.app.AppModel
 import hnau.common.model.goback.GlobalGoBackHandler
 import hnau.common.model.goback.GlobalGoBackHandlerImpl
@@ -26,65 +20,36 @@ import hnau.common.projector.utils.buildPrettyColorScheme
 import hnau.common.projector.utils.provideDynamicColorsGenerator
 import hnau.common.projector.utils.system
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.flow.StateFlow
 
 class AppProjector<M, S, P>(
     scope: CoroutineScope,
-    model: AppModel<M, S>,
+    private val model: AppModel<M, S>,
     createProjector: (CoroutineScope, M, GlobalGoBackHandler) -> P,
     private val content: @Composable (P) -> Unit,
 ) {
 
-    private data class State<P>(
-        val appContext: AppContext,
-        val projector: P,
+    private val projector = createProjector(
+        scope,
+        model.model,
+        GlobalGoBackHandlerImpl(model.goBackHandler),
     )
-
-    private val projector: StateFlow<Loadable<State<P>>> = model
-        .state
-        .mapWithScope(scope) { modelScope, stateOrLoading ->
-            stateOrLoading.map { state ->
-                val projector = createProjector(
-                    modelScope,
-                    state.model,
-                    GlobalGoBackHandlerImpl(model.goBackHandler),
-                )
-                State(
-                    appContext = state.context,
-                    projector = projector,
-                )
-            }
-        }
-
-    @Composable
-    fun Content() {
-        val state = projector
-            .collectAsState()
-            .value
-            .valueOrElse { return }
-        Content(
-            appContext = state.appContext,
-            projector = state.projector,
-        )
-    }
 
     private val dynamicColorsGenerator: DynamicColorsGenerator? =
         provideDynamicColorsGenerator()
 
     @Composable
-    private fun Content(
-        appContext: AppContext,
-        projector: P,
-    ) {
+    fun Content() {
 
-        val brightness: ThemeBrightness = appContext
+        val brightness: ThemeBrightness = model
+            .appContext
             .brightness
             .value
             .collectAsState()
             .value
             ?: ThemeBrightness.system
 
-        val colorScheme = appContext
+        val colorScheme = model
+            .appContext
             .tryUseSystemHue
             .value
             .collectAsState()
@@ -92,7 +57,8 @@ class AppProjector<M, S, P>(
             .ifTrue { dynamicColorsGenerator }
             ?.generateDynamicColors(brightness)
             .ifNull {
-                val hue = appContext
+                val hue = model
+                    .appContext
                     .fallbackHue
                     .value
                     .collectAsState()
